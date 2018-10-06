0aef9d7b593aa38c3c7eba3ceed53b7afc2d22c1
Sergej Herbert <herbert.sergej@gmail.com>
2018-10-06 09:45:07 +0200

    added .gitconfig

--- update-git.sh
+++ update-git.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 cp ~/.zshrc .zshrc
-cp ~/.tmux.conf tmux.conf
+cp ~/.tmux.conf .tmux.conf
+cp ~/.gitconfig .gitconfig
+cp -r ~/.gitconfs/ .gitconfs/
 
 git add .
 message="automatic update of $date"
