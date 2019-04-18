f60e4ab4abe3177362c7850a10d4dd4b8f5f4bc6
Sergej Herbert <herbert.sergej@gmail.com>
2019-04-18 23:31:45 +0200

    add install software script
    this script isntall basic software usable on all unix computers
    and applies the configuration

--- configs/.bash_aliases
+++ configs/.bash_aliases
@@ -1,6 +1,5 @@
 # SSH Mounting
 alias mountPiHome='sshfs pi@steinberg.home-webserver.de:/ ~/pi-home -p 8022'
-alias mountPiEugen='sshfs pi@fruehling.home-webserver.de:/ ~/pi-eugen -p 8022'
 
 # PDF quality adjustment
 alias pdfEbookQuality='gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -sOutputFile=output.pdf'
---
+++ install-software.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y zsh docker curl vim git tmux fonts-powerline
+sudo apt-get upgrade -y
+sudo apt-get autoremove
+# install pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# install vundle (vim package manager)
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install node-version-manager (nvm)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+# install oh-my-zsh
+rm -rf ~/.oh-my-zsh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+
+# applying the configs, especially useful because oh-my-zsh overwrites stuff
+source ./apply-configs.sh
+
+# install vim plugins (after config copied)
+vim +PluginInstall +qall
