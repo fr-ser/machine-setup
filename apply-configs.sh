6754b48f1e40862c64501ac87833b5af3f218d73
Sergej Herbert <herbert.sergej@gmail.com>
2019-04-19 10:48:52 +0200

    Add custom git command: git graph

--- apply-configs.sh
+++ apply-configs.sh
@@ -2,6 +2,7 @@
 
 cp ./configs/.bash_aliases ~/.bash_aliases
 cp ./configs/.gitignore_global ~/.gitignore_global
+sudo cp ./configs/git-graph /usr/local/bin/git-graph
 cp ./configs/.zshrc ~/.zshrc
 cp ./configs/.zshenv ~/.zshenv
 cp ./configs/.vimrc ~/.vimrc
@@ -10,7 +11,7 @@ mkdir -p ~/.tmux/
 cp ./configs/.tmuxline_snapshot ~/.tmux/.tmuxline_snapshot
 cp ./configs/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.oh-my-zsh/custom/themes/
-cp ./configs/robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme 
+cp ./configs/robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme
 cp -r ./configs/.gitconfs/ ~/
 mkdir -p ~/.config/
 cp -r ./configs/powerline/ ~/.config/
--- update-configs.sh
+++ update-configs.sh
@@ -8,6 +8,7 @@ cp ~/.tmux.conf ./configs/.tmux.conf
 cp ~/.tmux/.tmuxline_snapshot ./configs/.tmuxline_snapshot
 cp ~/.gitconfig ./configs/.gitconfig
 cp ~/.gitignore_global ./configs/.gitignore_global
+cp /usr/local/bin/git-graph ./configs/git-graph
 cp ~/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme ./configs/robbyrussell.zsh-theme
 cp -r ~/.gitconfs/* ./configs/.gitconfs/
 cp -r ~/.config/powerline/* ./configs/powerline/
