fba519f6eae8296bb886cc6549b968e61e7bbe8d
Sergej Herbert <herbert.sergej@gmail.com>
2021-01-23 18:17:59 +0100

    Add packages to build python locally

--- install-software.sh
+++ install-software.sh
@@ -6,7 +6,13 @@ curr_dir=$(pwd)
 
 sudo apt update
 # basic utilities
-sudo apt install -y zsh curl sysstat jq vim git tmux fonts-powerline xsel ncdu htop dmenu direnv
+sudo apt install -y \
+    zsh curl sysstat jq vim git tmux fonts-powerline xsel ncdu htop dmenu direnv sqlite3 make
+# packages to build python
+sudo apt install --no-install-recommends build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev \
+    libreadline-dev libffi-dev wget python3-dev libsqlite3-dev libbz2-dev llvm xz-utils tk-dev \
+    libxml2-dev libxmlsec1-dev liblzma-dev
+
 sudo apt upgrade -y
 sudo apt autoremove -y
 
@@ -42,5 +48,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker || true
 sudo usermod -aG docker $USER
 echo "To run docker without sudo you need to log out and back in"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L \
+    "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
